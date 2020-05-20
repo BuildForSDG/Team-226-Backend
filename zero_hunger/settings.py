8987da8372cfb69636863485c7ae430495e63b20
Wadinga Leonard Ngonga <wadingaleonard@gmail.com>
2020-05-20 22:13:04 +0100

    feat: temporary enabling debug mode

--- zero_hunger/settings.py
+++ zero_hunger/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY", default="secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False)
+DEBUG = config("DEBUG", default=True)
 
 ALLOWED_HOSTS = [
     "farmvoice.herokuapp.com",
